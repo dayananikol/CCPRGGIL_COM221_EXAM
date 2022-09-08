12197bffe72252ccf43ad10b2390069b1678d53d
dayananikol <111488292+dayananikol@users.noreply.github.com>
2022-09-08 14:18:17 +0800

    Create App.java

---
+++ exam_1/App.java
@@ -0,0 +1,18 @@
+public class App {
+    public static void main(String[] args) {
+
+        Student me = new Student ();
+
+        me.surname = " Danga";
+        me.firstname = " Diana Nicole";
+        me.middleinitial = " D.";
+        me.dateOfBirth = " 05/07/2003";
+        me.studentNumber =  2022104741;
+        me.studentEmailAddress = " dangadd@students.national-u.edu.ph";
+        me.iamawesome =   true;
+        me.sayMyStudentNumber();
+        me.sayMyStudentEmailAddress();
+        me.sayIAmAwesome();
+       
+    }
+}
